2a1dcd78a4b9afa014c37ed5383f0d34a8cc3e53
helo-kity <vinhchugg@hotmail.com>
2025-02-14 06:01:04 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOO!!! ❤️</div>
+          <div className="my-4 text-4xl font-bold">WOOOO!!! I LOOK FORWARD TO SEEING YOU IN HONG KONG ❤️</div>
         </>
       ) : (
         <>
@@ -46,7 +46,7 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="flex items-center my-4 text-4xl">Will you be my valentine Janis?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
